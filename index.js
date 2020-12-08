c1b49ad4f8606b849e34deaf71e24947a0a1e2c9
Alexander <alexkolom27@gmail.com>
2020-12-08 22:21:32 +0100

    style: rename files with routes to [name].route.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 const port = process.env.PORT;
 
 // middlewares
-app.use(session);
 app.use(express.json());
+app.use(session);
 
 // routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/comment', require('./routes/comment'));
-app.use('/api/post', require('./routes/post'));
-app.use('/api/user', require('./routes/user'));
+app.use('/api/auth', require('./routes/auth.route'));
+app.use('/api/comment', require('./routes/comment.route'));
+app.use('/api/post', require('./routes/post.route'));
+app.use('/api/user', require('./routes/user.route'));
 
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
